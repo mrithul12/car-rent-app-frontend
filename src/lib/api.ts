2e88c0885663f7543bb9a4046b1a9f47753f1344
mrithul <mrithulmridhu6@gmail.com>
2025-05-29 13:21:57 +0530

    add .env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,22 +1,26 @@
-export const getProduct = async ()=>{
-   
-   const response = await fetch('https://car-rent-backend-anii.onrender.com/api/cars?populate=thumbnail');
-   const data =await response.json();
-   return data.data;
-}
+import { Car } from "@/components/types/products";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 
-export const getProductDetails = async (slug:string) =>{
-   const response = await fetch(`https://car-rent-backend-anii.onrender.com/api/cars?populate=thumbnail&populate=images&filters[slug][$eq]=${slug}`);
-   const data = await response.json();
-   return data.data;
-}
+export const getProduct = async () => {
+  const response = await fetch(
+    "https://car-rent-backend-anii.onrender.com/api/cars?populate=thumbnail"
+  );
+  const data = await response.json();
+  return data.data;
+};
 
-export const carFetch = async ()=>{
-   const response = await fetch('http://localhost:3000/api/cars')
-   const data =await response.json()
-   return data
-  
-}
+export const getProductDetails = async (slug: string) => {
+  const response = await fetch(
+    `https://car-rent-backend-anii.onrender.com/api/cars?populate=thumbnail&populate=images&filters[slug][$eq]=${slug}`
+  );
+  const data = await response.json();
+  return data.data;
+};
 
-
+export const carFetch = async (): Promise<Car[]> => {
+  const response = await fetch(`${BASE_URL}/api/cars`);
+  const data = await response.json();
+  return data;
+};
