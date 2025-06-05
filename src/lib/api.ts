dc5fdd688031c3ac229f9be84239920707dcb9c3
mrithul <mrithulmridhu6@gmail.com>
2025-06-05 18:10:28 +0530

    add api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,10 @@
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-
-const BASE_SRAPI_URL = process.env.NEXT_PUBLIC_API_BASE_URL 
+const BASE_STRAPI_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https:car-rent-backend-anii.onrender.com"
 
 export const getProduct = async () => {
-  const response = await fetch(
-    `${BASE_SRAPI_URL}/api/cars?populate=thumbnail`
-  );
+  const response = await fetch(`${BASE_STRAPI_URL}/api/cars?populate=thumbnail`);
   const data = await response.json();
 
   return data.data;
@@ -15,10 +12,8 @@ export const getProduct = async () => {
 
 export const getProductDetails = async (slug: string) => {
   const response = await fetch(
-    `${BASE_SRAPI_URL}/api/cars?populate=thumbnail&populate=images&filters[slug][$eq]=${slug}`
+    `${BASE_STRAPI_URL}/api/cars?populate=thumbnail&populate=images&filters[slug][$eq]=${slug}`
   );
   const data = await response.json();
   return data.data;
 };
-
-
