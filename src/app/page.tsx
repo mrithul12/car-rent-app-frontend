228fa0fe43c26033d79e0015eb78e276a30056af
mrithul <mrithulmridhu6@gmail.com>
2025-06-05 16:46:05 +0530

    add final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import Add from "@/components/advertaisement/adds";
+
 import Display from "@/components/startingPage/Display";
 import HomeDisply from "@/components/startingPage/homeDisply";
 import StartingPage from "@/components/startingPage/StartingPage";
@@ -6,7 +6,7 @@ import StartingPage from "@/components/startingPage/StartingPage";
 export default async function Home() {
   return (
     <div>
-      {/* <Add /> */}
+    
       <StartingPage/>
       <HomeDisply/>
       <Display/>
